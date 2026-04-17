3872af2565ef8184ff3ea4248228f34f45ac15cd
HET2912 <hetpatel29122004@gmail.com>
2026-04-17 16:46:10 +0530

    just changes

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const { Server } = require("socket.io");
 const app = require("./app");
 
 const PORT = process.env.PORT || 5000;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/finx";
+const MONGODB_URI = process.env.MONGODB_URI;
 
 const allowedOrigins = [process.env.FRONTEND_ORIGIN || "http://localhost:5173", "http://localhost:5174"];
 
